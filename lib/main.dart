10bf4c171e106c6b81d3a01eaec1c91a82bb96d5
Tima Gixe <timagixe@protonmail.com>
2021-03-07 17:41:13 +0200

    Add TextField widgets

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,9 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   final List<Transaction> transactions = [
     Transaction(
       title: 'Shoes',
@@ -53,6 +56,34 @@ class MyHomePage extends StatelessWidget {
               child: Card(
                 elevation: 2,
                 child: Text('CARD WITH DAYS'),
+              ),
+            ),
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: titleController,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: amountController,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        print(titleController.text);
+                        print(amountController.text);
+                      },
+                      child: Text(
+                        'Submit Transaction',
+                        style: TextStyle(color: Colors.purple),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
             Container(
