2fa6a452b34e2ec1324bb8b4dc8b0023190c944a
Tima Gixe <timagixe@protonmail.com>
2021-03-07 17:04:23 +0200

    Style transaction card

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          children: <Widget>[
+          children: [
             Container(
               width: double.infinity,
               child: Card(
@@ -59,7 +59,51 @@ class MyHomePage extends StatelessWidget {
               child: Column(
                 children: transactions
                     .map((transaction) => Card(
-                          child: Text(transaction.title),
+                          child: Row(
+                            children: [
+                              Container(
+                                constraints: BoxConstraints(
+                                  minWidth: 100,
+                                ),
+                                margin: EdgeInsets.symmetric(
+                                  vertical: 10,
+                                  horizontal: 15,
+                                ),
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.purple,
+                                    width: 2,
+                                  ),
+                                ),
+                                child: Text(
+                                  '\$${transaction.amount.toString()}',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.purple,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    transaction.title,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Text(transaction.date.toString(),
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      ))
+                                ],
+                              )
+                            ],
+                          ),
                         ))
                     .toList(),
               ),
