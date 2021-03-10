9b9bcac00059dc578911d3dd930ea49df82b39e2
Tima Gixe <timagixe@protonmail.com>
2021-03-10 10:34:21 +0200

    Use ExpensesChartBar in ExpensesChart widget

--- lib/widgets/expenses_chart.dart
+++ lib/widgets/expenses_chart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:personal_expenses/widgets/expenses_chart_bar.dart';
 
 import '../models/transaction.dart';
 
@@ -8,10 +9,11 @@ class ExpensesChart extends StatelessWidget {
 
   ExpensesChart(this.recentTransactions);
 
-  List<Map<String, Object>> get getGrouppedTransactions {
+  List<Map<String, Object>> get grouppedTransactions {
     return List.generate(7, (index) {
       final DateTime weekDay = DateTime.now().subtract(Duration(days: index));
-      final String weekDayShortcut = DateFormat.E().format(weekDay);
+      final String weekDayShortcut =
+          DateFormat.E().format(weekDay).substring(0, 1);
 
       var totalAmount = 0.0;
 
@@ -35,11 +37,33 @@ class ExpensesChart extends StatelessWidget {
     });
   }
 
+  double get totalSpending {
+    return grouppedTransactions.fold(
+        0.0, (previousValue, element) => previousValue += element['amount']);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
-      child: Row(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: grouppedTransactions
+              .map((value) => Flexible(
+                    fit: FlexFit.tight,
+                    child: ExpensesChartBar(
+                      weekDayLabel: value['day'],
+                      spentAmountTotal: value['amount'],
+                      percentageOfSpentAmountTotal: totalSpending != 0.0
+                          ? (value['amount'] as double) / totalSpending
+                          : totalSpending,
+                    ),
+                  ))
+              .toList(),
+        ),
+      ),
     );
   }
 }
