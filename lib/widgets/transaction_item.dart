9252ecd5e0c65d95415cda626184ce530bffd22d
Tima Gixe <timagixe@protonmail.com>
2021-03-10 10:48:47 +0200

    Refactor TransactionItem widget

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -15,47 +15,25 @@ class TransactionItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
-        children: [
-          Container(
-            constraints: BoxConstraints(
-              minWidth: 100,
-            ),
-            margin: EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 15,
-            ),
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Theme.of(context).primaryColor,
-                width: 2,
-              ),
-            ),
-            child: Text(
-              '\$${amount.toStringAsFixed(2)}',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.subtitle1.copyWith(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                  color: Theme.of(context).primaryColor),
+      elevation: 5,
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Padding(
+            padding: EdgeInsets.all(5),
+            child: FittedBox(
+              child: Text('\$${amount.toStringAsFixed(2)}'),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: Theme.of(context).textTheme.headline2,
-              ),
-              Text(
-                DateFormat.yMMMd().format(date),
-                // style: Theme.of(context).textTheme.headline2,
-                style: Theme.of(context).textTheme.subtitle1,
-              )
-            ],
-          )
-        ],
+          radius: 30,
+        ),
+        title: Text(
+          title,
+          style: Theme.of(context).textTheme.headline2,
+        ),
+        subtitle: Text(
+          DateFormat.yMMMd().format(date),
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
       ),
     );
   }
