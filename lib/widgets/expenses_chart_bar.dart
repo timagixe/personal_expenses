b615ae242d5611499b0007bdc113fdd43fd165df
Tima Gixe <timagixe@protonmail.com>
2021-03-10 10:33:53 +0200

    Creat ExpensesChartBar widget

---
+++ lib/widgets/expenses_chart_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class ExpensesChartBar extends StatelessWidget {
+  final String weekDayLabel;
+  final double spentAmountTotal;
+  final double percentageOfSpentAmountTotal;
+
+  ExpensesChartBar({
+    @required this.percentageOfSpentAmountTotal,
+    @required this.spentAmountTotal,
+    @required this.weekDayLabel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FittedBox(
+          child: Text('\$${spentAmountTotal.toStringAsFixed(0)}'),
+        ),
+        SizedBox(height: 4),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1,
+                  ),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentageOfSpentAmountTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 4),
+        Text(weekDayLabel)
+      ],
+    );
+  }
+}
