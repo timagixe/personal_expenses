12c03b48591aa50926a02cf1fdcbeaba0fba2999
Tima Gixe <timagixe@protonmail.com>
2021-03-11 10:27:02 +0200

    Use LayoutBuilder in ExpensesChartBar widget

--- lib/widgets/expenses_chart_bar.dart
+++ lib/widgets/expenses_chart_bar.dart
@@ -13,45 +13,63 @@ class ExpensesChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 20,
-          child: FittedBox(
-            child: Text('\$${spentAmountTotal.toStringAsFixed(0)}'),
+    return LayoutBuilder(builder: (context, constraints) {
+      final double textWidgetHeight =
+          constraints.heightConstraints().maxHeight * 0.15;
+      final double sizedBoxWidgetHeight =
+          constraints.heightConstraints().maxHeight * 0.05;
+      final double chartBarHeight =
+          constraints.heightConstraints().maxHeight * 0.6;
+
+      return Column(
+        children: [
+          Container(
+            height: textWidgetHeight,
+            child: FittedBox(
+              child: Text('\$${spentAmountTotal.toStringAsFixed(0)}'),
+            ),
           ),
-        ),
-        SizedBox(height: 4),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1,
-                  ),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: percentageOfSpentAmountTotal,
-                child: Container(
+          SizedBox(
+            height: sizedBoxWidgetHeight,
+          ),
+          Container(
+            height: chartBarHeight,
+            width: 10,
+            child: Stack(
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1,
+                    ),
+                    color: Color.fromRGBO(220, 220, 220, 1),
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-              ),
-            ],
+                FractionallySizedBox(
+                  heightFactor: percentageOfSpentAmountTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        SizedBox(height: 4),
-        Text(weekDayLabel)
-      ],
-    );
+          SizedBox(
+            height: sizedBoxWidgetHeight,
+          ),
+          Container(
+            height: textWidgetHeight,
+            child: FittedBox(
+              child: Text(weekDayLabel),
+            ),
+          )
+        ],
+      );
+    });
   }
 }
